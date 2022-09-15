6a20f38c5912a452cd8bab84688180735138575c
naufal zhafari <zhafarinaufal@gmail.com>
2022-09-15 12:18:09 +0700

    Telah Mengcommit

--- wishlist/views.py
+++ wishlist/views.py
@@ -27,20 +27,10 @@ def show_json(request):
 
 def show_json_by_id(request,id):
     data = BarangWishlist.objects.filter(pk=id)
-
-    if (id == 1) :
-        return HttpResponse(serializers.serialize("json", data), content_type="application/json")
-    elif (id == 2) :
-        return HttpResponse(serializers.serialize("json", data), content_type="application/json")
-    elif (id == 3) :
-        return HttpResponse(serializers.serialize("json", data), content_type="application/json")
-
+    
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+    
 def show_xml_by_id(request,id):
     data = BarangWishlist.objects.filter(pk=id)
 
-    if (id == 1) :
-        return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
-    elif (id == 2) :
-        return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
-    elif (id == 3) :
-        return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")

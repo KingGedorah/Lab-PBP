e01e7ac1b92e09583541d192a9b876995ef81c45
naufal zhafari <zhafarinaufal@gmail.com>
2022-10-06 14:56:28 +0700

    Lab 5

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -7,6 +7,8 @@ from wishlist.views import show_xml_by_id
 from wishlist.views import register
 from wishlist.views import login_user
 from wishlist.views import logout_user
+from wishlist.views import show_wishlist_ajax
+
 
 app_name = 'wishlist'
 
@@ -18,5 +20,6 @@ urlpatterns = [
     path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
-    path('logout/', logout_user, name='logout')
+    path('logout/', logout_user, name='logout'),
+    path('ajax/', show_wishlist_ajax, name='show_wishlist_ajax'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -22,7 +22,15 @@ def show_wishlist(request):
     'last_login': request.COOKIES['last_login'],
     }
     return render(request, "wishlist.html", context)
-    
+
+@login_required(login_url='/wishlist/login/')
+def show_wishlist_ajax(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+    'list_barang': data_barang_wishlist,
+    'nama': 'Naufal',
+    }
+    return render(request, "wishlist_ajax.html", context)
     
 def show_xml(request):
     data = BarangWishlist.objects.all()
@@ -77,3 +85,19 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('wishlist:login'))
     response.delete_cookie('last_login')
     return response
+
+def get_wishlist_data(request):
+    wish_list= BarangWishlist.objects.all()
+    return JsonResponse({"wish_list":list(wish_list.values())})
+
+@login_required(login_url='/wishlist/login/')
+def create_item(request):
+    form = NewItem()
+    context = {'form': form}
+    if request.method == 'POST':
+        form = NewItem(request.POST)
+        if form.is_valid():
+            form_listener = form.save(commit=False)
+            form_listener.user = request.user
+            form_listener.save()
+            return HttpResponse(reverse('wishlist:show_wishlist_ajax'))
